770ab8cd33a4c95041b524eab51b8234fcbae7a5
IoTPanic <samuel.archibald00@gmail.com>
2020-05-02 15:05:47 -0400

    Added updated database structures/drop table fn

--- internal/db/delete.go
+++ internal/db/delete.go
@@ -1,1 +1,17 @@
 package db
+
+import "fmt"
+
+// DeleteTable drops table from instance
+func DeleteTable(tablename string) error {
+	stmt, err := db.Prepare(fmt.Sprintln("DROP TABLE %s", tablename))
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec()
+
+	if err != nil {
+		return err
+	}
+	return err
+}
--- internal/db/objects.go
+++ internal/db/objects.go
@@ -1,26 +1,59 @@
 package db
 
-type Fixture struct {
-	ID               int64  `sql:"id"`
-	Name             string `sql:"name"`
-	LongID           string `sql:"longID"`
-	PixelsID         int    `sql:"pixelsID"`
-	UniverseID       int    `sql:"universeID"`
-	ConnectionMethod string `sql:"connectionMethod"`
-	ConnectionHost   string `sql:"connectionHost"`
-	ConnectionPort   int    `sql:"connectionPort"`
-	ConnectionMQTT   bool   `sql:"connectionWMQTT"`
+import "time"
+
+// Device pixelpusher object for DB
+type Device struct {
+	ID        int64  `sql:"id"`
+	Name      string `sql:"name"`
+	LongID    string `sql:"longID"`
+	Hostname  string `sql:"hostname"`
+	Port      int    `sql:"port"`
+	Connector bool   `sql:"connector"`
+	Key       string `sql:"key"`
+	UseKey    bool   `sql:"use_key"`
 }
 
+// Channel pixelpusher object for DB
 type Channel struct {
-	ID       int  `sql:"id"`
-	Channel  int  `sql:"channel"`
-	DeviceID int  `sql:"deviceID"`
-	RGBW     bool `sql:"RGBW"`
-	Length   int  `sql:"length"`
+	ID        int    `sql:"id"`
+	Name      string `sql:"name"`
+	Type      int    `sql:"type"`
+	Device    int    `sql:"device"`
+	Color     string `sql:"color"`
+	MaxLength int    `sql:"max_length"`
 }
 
-type Universe struct {
-	ID   int    `sql:"id"`
-	Name string `sql:"name"`
+// Matrix pixelpusher object for DB
+type Matrix struct {
+	ID         int    `sql:"id"`
+	Device     int    `sql:"device"`
+	Channel    int    `sql:"channel"`
+	Width      int    `sql:"width"`
+	Height     int    `sql:"height"`
+	Type       int    `sql:"type"`
+	Coloring   string `sql:"coloring"`
+	Offset     int    `sql:"offset"`
+	Brightness int    `sql:"brightness"`
+}
+
+// Project pixelpusher object for DB
+type Project struct {
+	ID            int       `sql:"id"`
+	Name          string    `sql:"name"`
+	Created       time.Time `sql:"created"`
+	LastUpdate    time.Time `sql:"last_update"`
+	Client        string    `sql:"client"`
+	Active        bool      `sql:"active"`
+	FrontendState string    `sql:"frontend_state"`
+}
+
+// User pixelpusher object for DB
+type User struct {
+	ID        int       `sql:"id"`
+	Name      string    `sql:"name"`
+	Username  string    `sql:"username"`
+	Password  string    `sql:"password"`
+	Created   time.Time `sql:"created"`
+	LastLogin time.Time `sql:"last_login"`
 }
